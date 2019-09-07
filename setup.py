03f8dfdff5aaa6b3afbc0348816e771c91a34c22
Kaleb McKinney <kalebmckinney@outlook.com>
2019-09-07 11:07:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="andromeda",
-    version="1.0.0-pr",
+    version="1.0.0",
     author="Kaleb McKinney",
     description="Quickly create large amounts of unique serial numbers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mckkaleb/andromeda",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.4',
 )
