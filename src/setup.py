c630b6c2683ecb17bda534468d591b72a74c0c92
Kaleb McKinney <kalebmckinney@outlook.com>
2019-09-11 16:30:03 -0400

    change project name

--- src/setup.py
+++ src/setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nightsky",
-    version="1.0.1",
+    name="blueshift",
+    version="1.0.0",
     author="Kaleb McKinney",
     description="Quickly create large amounts of unique serial numbers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mckkaleb/andromeda",
+    url="https://github.com/mckkaleb/blueshift",
     packages=setuptools.find_packages(),
     python_requires='>=3.4',
 )
