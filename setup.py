90e47babb0ad7f19151ef1214fc1d3fa33b04223
Kaleb McKinney <kalebmckinney@outlook.com>
2019-10-14 17:23:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="blueshift",
-    version="0.0.1",
+    version="0.0.2",
     author="Kaleb McKinney",
     description="Quickly create large amounts of unique serial numbers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mckkaleb/blueshift",
-    download_url="https://github.com/mckkaleb/blueshift/archive/0.0.1.tar.gz",
+    download_url="https://github.com/mckkaleb/blueshift/archive/0.0.2.tar.gz",
     packages=['blueshift'],
     python_requires='>=3.4',
     classifiers=[
